86b16cdb3eb530c130581b215beadd7cb9e8f7e3
yjb23tech <yjb23tech@gmail.com>
2023-12-01 20:54:52 +0000

    Should work lol

--- App.jsx
+++ App.jsx
@@ -6,8 +6,38 @@ const App = () => {
         <div>
             <h1>Above It All</h1>
             <Card 
+                song_cover_art="track_9_song_art.png" 
+                song_title="Hell Of A Life"
+                album_icon="album_icon.png"
+                album_name="My Beautiful Dark Twisted Fantasy"
+                artist_icon="artist_icon.png"
+                artist_name="Kanye West"
+                genre_icon="genre_icon.png"
+                genre_name="Baroque Rap"
+            />
+            <Card
                 song_cover_art="track_10_song_art.png" 
                 song_title="Blame Game"
+                album_icon="album_icon.png"
+                album_name="My Beautiful Dark Twisted Fantasy"
+                artist_icon="artist_icon.png"
+                artist_name="Kanye West"
+                genre_icon="genre_icon.png"
+                genre_name="Baroque Rap"
+            />
+            <Card 
+                song_cover_art="track_11_song_art.png" 
+                song_title="Lost In The World"
+                album_icon="album_icon.png"
+                album_name="My Beautiful Dark Twisted Fantasy"
+                artist_icon="artist_icon.png"
+                artist_name="Kanye West"
+                genre_icon="genre_icon.png"
+                genre_name="Baroque Rap"
+            />
+            <Card 
+                song_cover_art="track_12_song_art.png" 
+                song_title="Who Will Survive In America?"
                 album_icon="album_icon.png"
                 album_name="My Beautiful Dark Twisted Fantasy"
                 artist_icon="artist_icon.png"
