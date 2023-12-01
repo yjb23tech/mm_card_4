250b0560daee5d807594e9ebf6c6b21cfe2a1446
yjb23tech <yjb23tech@gmail.com>
2023-12-01 20:12:09 +0000

    New Bxne

--- App.jsx
+++ App.jsx
@@ -1,9 +1,11 @@
 import { createRoot } from "react-dom/client"
+import Card from "./components/Card.jsx"
 
 const App = () => {
     return (
         <div>
             <h1>Above It All</h1>
+            <Card song_cover_art="track_10_song_art.png" />
         </div>
     )
 };
